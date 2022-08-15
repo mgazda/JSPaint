b64803247a577f679f1066e07f3aaf315ed8d267
Marcin Gazda <marcin.gazda@gmail.com>
2022-08-15 10:58:22 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", run);
+
+function run() {
+    const appElement = document.querySelector("#app")
+    for (let i=0; i<100; i++) {
+       const node = document.createElement("div")
+       node.setAttribute("class", "box")
+       node.onmouseenter = node.onmousedown = setActive
+       appElement.appendChild(node)
+    } 
+
+    console.log("running...")
+}
+
+function setActive(e) {
+    if (e.buttons == 0) {
+        return
+    }
+    const node = e.target
+    const classes = node.classList
+
+    if (classes.contains("active")) {
+        classes.remove("active")
+    } else {
+        classes.add("active")
+    }
+}
